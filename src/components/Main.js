fb2c515650c72a735d4928eecef4e0ca787258f5
Artur Carvalho <artur.carvalho0@outlook.com>
2023-09-07 14:32:21 -0300

    Adicionando tarefas

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,11 +6,21 @@ import { FaPlus, FaEdit, FaWindowClose } from 'react-icons/fa';
 export default class Main extends Component {
   state = {
     novaTarefa: '',
-    tarefas: [
-      'Fazer café',
-      'Beber água',
-      'Estudar,',
-    ],
+    tarefas: [],
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { tarefas } = this.state;
+    let { novaTarefa } = this.state;
+    novaTarefa = novaTarefa.trim();
+
+    if (tarefas.indexOf(novaTarefa) !== -1) return;
+
+    const novasTarefas = [...tarefas];
+    this.setState({
+      tarefas: [...novasTarefas, novaTarefa],
+    });
   };
 
   handleChange = (e) => {
@@ -25,7 +35,7 @@ export default class Main extends Component {
     return (
       <div className="main">
         <h1>Lista de tarefas</h1>
-        <form action="#" className="form">
+        <form onSubmit={this.handleSubmit} action="#" className="form">
           <input onChange={this.handleChange} type="text" value={novaTarefa} />
           <button type="submit">
             <FaPlus />
@@ -37,10 +47,10 @@ export default class Main extends Component {
             tarefas.map((tarefa) => (
               <li key={tarefa}>
                 {tarefa}
-                <div>
+                <span>
                   <FaEdit className="edit" />
                   <FaWindowClose className="delete" />
-                </div>
+                </span>
               </li>
             ))
           }
